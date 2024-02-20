d106308d44130a31db6af0f65240de62d18cc0f5
Igor Pudovkin <iggpd62@yandex.ru>
2024-02-20 05:18:09 +0300

    two

--- task_1.swift
+++ task_1.swift
@@ -1,4 +1,6 @@
-// Функция для расчета суммы по вкладу
+// Написать функцию, которая на вход принимает число: сумма, которую пользователь хочет положить на вклад, следующий аргумент это годовой процент, 
+// третий аргумент это срок Функция возвращает сколько денег получит пользователь по итогу.
+
 func calculateDepositAmount(depositAmount: Double, annualInterestRate: Double, term: Int) -> Double {
     var totalAmount = depositAmount
     for _ in 1...term {
